41eb2711e3a928f36bb9db0140c3978647e56ad0
inderpreet deol <200404339@student.georgianc.on.ca>
2020-01-20 14:04:02 -0500

    first

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string details(string id)
+        {
+            string message = "this is the Product = " + id ;
+            return message;
+        }
+    }
+}
